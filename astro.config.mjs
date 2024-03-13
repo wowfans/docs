f1fbfb34de9e001d17b1ffb49c5cb351633fc891
wolone <71954466@qq.com>
2024-03-13 20:02:04 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://wowfans.github.io/wowfans.cn',
+	site: 'https://docs.wowfans.cn',
 	integrations: [
 		starlight({
 			title: 'WOWFANS',
